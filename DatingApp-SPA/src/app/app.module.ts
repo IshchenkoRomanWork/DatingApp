77c6f48c7541749ba7e756ac2ab4dfa0e716c417
Роман Ищенко <mahodzi@gmail.com>
2020-03-02 13:14:01 +0200

    End of section 13

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -8,11 +8,12 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { appRoutes } from './routes';
 import { AuthService } from './_services/auth.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { FileUploadModule } from 'ng2-file-upload';
+// import {TimeAgoPipe} from 'time-ago-pipe';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -37,6 +38,12 @@ export function tokenGetter()
    return localStorage.getItem('token');
 }
 
+// @Pipe({
+//    name: 'timeAgo',
+//    pure: false
+// })
+// export class TimeAgoExtendsPipe extends TimeAgoPipe {}
+
 @NgModule({
    declarations: [
       AppComponent,
@@ -51,6 +58,7 @@ export function tokenGetter()
       // NgxGalleryModule,
       MemberEditComponent,
       PhotoEditorComponent,
+      // TimeAgoExtendsPipe,
    ],
    imports: [
       BrowserModule,
